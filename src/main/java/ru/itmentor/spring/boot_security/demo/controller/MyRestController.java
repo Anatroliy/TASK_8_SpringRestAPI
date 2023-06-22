4d5c500c7274a255aae69fa30c87e77a9a0de46a
Anatoliy <rememberw8h8@yandex.ru>
2023-06-23 00:57:51 +0300

    complited task

--- src/main/java/ru/itmentor/spring/boot_security/demo/controller/MyRestController.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/controller/MyRestController.java
@@ -2,9 +2,7 @@ package ru.itmentor.spring.boot_security.demo.controller;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.itmentor.spring.boot_security.demo.entity.Role;
 import ru.itmentor.spring.boot_security.demo.entity.User;
 import ru.itmentor.spring.boot_security.demo.service.RoleService;
@@ -25,10 +23,27 @@ public class MyRestController {
         List<User> users = userService.findAllUsers();
         return users;
     }
+
     @GetMapping("/roles")
-    public List<Role> allRoles(){
+    public List<Role> allRoles() {
         List<Role> roles = new ArrayList<>(roleService.getAllRoles());
         return roles;
     }
+
+    @PostMapping("/newUser")
+    public User addUser(@RequestBody User user) {
+        userService.saveUser(user);
+        return user;
+    }
+    @PutMapping("/edit")
+    public User updateUser(@RequestBody User user){
+        userService.updateUser(user);
+        return user;
+    }
+    @DeleteMapping("/user/{id}")
+    public void deleteUserById(@PathVariable("id") long id){
+        userService.deleteUserById(id);
+    }
+
 
 }
